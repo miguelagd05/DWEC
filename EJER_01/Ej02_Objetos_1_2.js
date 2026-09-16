f91b9b489c641e50934f0c4b0dcff735c41740ac
miguelagd05 <miguelagd05@educastur.es>
2026-09-16 11:12:22 +0200

    Segundo ejercicio

---
+++ EJER_01/Ej02_Objetos_1_2.js
@@ -0,0 +1,21 @@
+// 1. Crea un objeto ‘coche’ con las propiedades: ‘marca’ (string), ‘modelo’ (string), ‘año’ (number) y ‘estaDisponible’ (boolean).
+
+const coche =  {
+    marca: Hyundai
+    modelo: SUV
+    año = 2024
+    estaDisponible = true
+}
+// 2. Muestra el objeto completo en consola utilizando console.table().
+
+console.table(coche)
+
+// 3. Usa la desestructuración (destructuring) para extraer la ‘marca’ y el ‘modelo’ en variables separadas e imprímelas en consola.
+
+// 4. Cambia el valor de la propiedad ‘estaDisponible’ a ‘true’.
+
+// 5. Agrega una nueva propiedad ‘color’ al objeto.
+
+// 6. Elimina la propiedad ‘año’ del objeto.
+
+//7. Vuelve a imprimir el objeto modificado en consola.
